d95f45d0dde8141af4c352c431a8625d952e17bd
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-05-18 22:45:19 -0300

    refactor: componentizing home page

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <p className="text-sm leading-relaxed text-gray-200 ">
+      Feito com 💜 no NLW da Rocketseat pelo{' '}
+      <a
+        target="_blank"
+        href="https://github.com/mauriciocr22"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Maurício
+      </a>
+    </p>
+  )
+}
